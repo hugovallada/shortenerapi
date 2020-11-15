735f818483d2b33283b74d8e4d0dd4b36aac2463
Hugo Vallada <valladahugo@gmail.com>
2020-11-15 11:30:28 -0300

    Remoçao de funçoes nao utilizadas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import {create, redirecionar, encurtar, listarUrls} from './controllers/UrlController.js';
+import {encurtar, listarUrls, redirecionar} from './controllers/UrlController.js';
 
 const router = express.Router();
 
-router.post('/', create);
 router.get('/', listarUrls);
 router.get('/:short', redirecionar);
 router.post('/encurtar', encurtar);
